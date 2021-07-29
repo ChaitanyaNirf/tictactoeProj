9bc4b8de416afdaad83bbb4f7dab69ebe99f9001
ChaitanyaNirf <87872295+ChaitanyaNirf@users.noreply.github.com>
2021-07-29 08:58:55 +0530

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -73,12 +73,12 @@ reset.addEventListener("click", function (e) {
 let counter = 9;
 
 c00.addEventListener("click", function (e) {
-//     console.log("You clicked c00")
+    // console.log("You clicked c00")
     if (isEmpty(object.c00) && !check()) {
         input = !input;
         object.c00 = input;
         audio.play();
-//         console.log(object.c00)
+        // console.log(object.c00)
         symbol(object.c00, c00)
         counter = counter - 1;
         check();
@@ -86,96 +86,96 @@ c00.addEventListener("click", function (e) {
 
 })
 c01.addEventListener("click", function (e) {
-//     console.log("You clicked c01")
+    // console.log("You clicked c01")
     if (isEmpty(object.c01) && !check()) {
         input = !input;
         object.c01 = input;
         audio.play();
-//         console.log(object.c01)
+        // console.log(object.c01)
         symbol(object.c01, c01)
         counter = counter - 1;
         check();
     }
 })
 c02.addEventListener("click", function (e) {
-//     console.log("You clicked c02")
+    // console.log("You clicked c02")
     if (isEmpty(object.c02) && !check()) {
         input = !input;
         object.c02 = input;
         audio.play();
-//         console.log(object.c02)
+        // console.log(object.c02)
         symbol(object.c02, c02);
         counter = counter - 1;
         check();
     }
 })
 c10.addEventListener("click", function (e) {
-//         console.log("You clicked c10")
     if (isEmpty(object.c10) && !check()) {
+        // console.log("You clicked c10")
         input = !input;
         object.c10 = input;
         audio.play();
-//         console.log(object.c10)
+        // console.log(object.c10)
         symbol(object.c10, c10)
         counter = counter - 1;
         check();
     }
 })
 c11.addEventListener("click", function (e) {
-//     console.log("You clicked c11")
+    // console.log("You clicked c11")
     if (isEmpty(object.c11) && !check()) {
         input = !input;
         object.c11 = input;
         audio.play();
-//         console.log(object.c11)
+        // console.log(object.c11)
         symbol(object.c11, c11)
         counter = counter - 1;
         check();
     }
 })
 c12.addEventListener("click", function (e) {
-//     console.log("You clicked c12")
+    // console.log("You clicked c12")
     if (isEmpty(object.c12) && !check()) {
         input = !input;
         object.c12 = input;
         audio.play();
-//         console.log(object.c12)
+        // console.log(object.c12)
         symbol(object.c12, c12)
         counter = counter - 1;
         check();
     }
 })
 c20.addEventListener("click", function (e) {
-//     console.log("You clicked c20")
+    // console.log("You clicked c20")
     if (isEmpty(object.c20) && !check()) {
         input = !input;
         object.c20 = input;
         audio.play();
-//         console.log(object.c20)
+        // console.log(object.c20)
         symbol(object.c20, c20)
         counter = counter - 1;
         check();
     }
 })
 c21.addEventListener("click", function (e) {
-//     console.log("You clicked c21")
+    // console.log("You clicked c21")
     if (isEmpty(object.c21) && !check()) {
         input = !input;
         object.c21 = input;
         audio.play();
-//         console.log(object.c21)
+        // console.log(object.c21)
         symbol(object.c21, c21)
         counter = counter - 1;
         check();
     }
 })
 c22.addEventListener("click", function (e) {
-//     console.log("You clicked c22")
+    // console.log("You clicked c22")
     if (isEmpty(object.c22) && !check()) {
         input = !input;
         object.c22 = input;
         audio.play();
-//         console.log(object.c22)
+        // console.log(object.c22)
         symbol(object.c22, c22)
         counter = counter - 1;
         check();
@@ -188,13 +188,13 @@ function check() {
         if (object.c01 == object.c02) {
             // console.log("enter")
             if (object.c01 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 audioOverall.pause();
                 audioWin.play();
                 message.innerHTML = '( o ) won !!!'
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioWin.play();
             }
@@ -205,13 +205,13 @@ function check() {
     if (object.c10 == object.c11) {
         if (object.c11 == object.c12) {
             if (object.c11 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 audioOverall.pause();
                 message.innerHTML = '( o ) won !!!'
                 audioWin.play();
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
@@ -223,13 +223,13 @@ function check() {
     if (object.c20 == object.c21) {
         if (object.c21 == object.c22) {
             if (object.c21 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 message.innerHTML = '( o ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
@@ -241,13 +241,13 @@ function check() {
     if (object.c00 == object.c11) {
         if (object.c11 == object.c22) {
             if (object.c11 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 message.innerHTML = '( o ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
@@ -259,13 +259,13 @@ function check() {
     if (object.c02 == object.c11) {
         if (object.c11 == object.c20) {
             if (object.c11 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 message.innerHTML = '( o ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
@@ -277,13 +277,13 @@ function check() {
     if (object.c00 == object.c10) {
         if (object.c10 == object.c20) {
             if (object.c10 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 message.innerHTML = '( o ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
@@ -295,13 +295,13 @@ function check() {
     if (object.c01 == object.c11) {
         if (object.c11 == object.c21) {
             if (object.c11 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 message.innerHTML = '( o ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
@@ -313,13 +313,13 @@ function check() {
     if (object.c02 == object.c12) {
         if (object.c12 == object.c22) {
             if (object.c12 == true) {
-//                 console.log("p2 won");
+                // console.log("p2 won");
                 message.innerHTML = '( o ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
             }
             else {
-//                 console.log("P1 won");
+                // console.log("P1 won");
                 message.innerHTML = '( x ) won !!!'
                 audioOverall.pause();
                 audioWin.play();
